bdbda988af8351de774942c77910bd245d07a785
tjulioh <thiagojulio20@gmail.com>
2021-07-15 15:40:37 -0300

    Atualização do Desafio 4

--- desafio/4/Programa.java
+++ desafio/4/Programa.java
@@ -2,7 +2,7 @@ public class Programa {
     public static void main(String[] args) {
         Funcionario funcionario = new Funcionario(5087,"Thiago JUlio", "Rua Domingos Zanatta", "04801239123", "1204129", "5432523",ESexo.MASCULINO, 3980.50, 3450.70);
         Fornecedor fornecedor = new Fornecedor(1,"Agua Sistemas", "Rua Nereu Ramos", "04845645645","35432523","4124","501234",null,null);
-        Cliente cliente = new Cliente(1,"Criciuma", "Rua Domingos Zanatta", "048912414","35432523",ESexo.FEMININO,1651.0,1297.0);
+        Cliente cliente = new Cliente(1,"Criciuma", "Rua Domingos Zanatta", "048912414","35432523","19383",ESexo.FEMININO,1651.0,1297.0);
 
         System.out.println("Cliente Limite: " + cliente.getLimiteUtilizado() + "/" + cliente.getLimiteCrediario());        
         
