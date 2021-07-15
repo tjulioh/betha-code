a6bec5ad883c80737cc32e37aa5a32bf72db4d62
Thiago Julio <thiagojulio20@gmail.com>
2021-07-15 15:36:51 -0300

    Atualização do Desafio 4

--- desafio/4/Programa.java
+++ desafio/4/Programa.java
@@ -1,8 +1,11 @@
 public class Programa {
     public static void main(String[] args) {
-        Funcionario funcionario = new Funcionario(5087,"Thiago JUlio", "Rua Domingos Zanatta", "5501239123", "1204129", "5432523",ESexo.MASCULINO, 3980.50, 3450.70);
-        Fornecedor fornecedor = new Fornecedor(1,"Agua Sistemas", "Rua Nereu Ramos", "45645645","35432523","4124","501234",null,null);
+        Funcionario funcionario = new Funcionario(5087,"Thiago JUlio", "Rua Domingos Zanatta", "04801239123", "1204129", "5432523",ESexo.MASCULINO, 3980.50, 3450.70);
+        Fornecedor fornecedor = new Fornecedor(1,"Agua Sistemas", "Rua Nereu Ramos", "04845645645","35432523","4124","501234",null,null);
+        Cliente cliente = new Cliente(1,"Criciuma", "Rua Domingos Zanatta", "048912414","35432523",ESexo.FEMININO,1651.0,1297.0);
 
+        System.out.println("Cliente Limite: " + cliente.getLimiteUtilizado() + "/" + cliente.getLimiteCrediario());        
+        
         System.out.println("Salário Bruto: "+ funcionario.getSalarioBruto());
         funcionario.dissidioAplicar(7.5);
         System.out.println("Salário Bruto com Dissídio: "+ funcionario.getSalarioBruto());
