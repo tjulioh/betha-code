5e7e5e65671f3686786aa4f93c42ee9ada3d0227
tjulioh <thiagojulio20@gmail.com>
2021-07-31 11:41:19 -0300

    Adição da Aula 31/07/2021

---
+++ aula/2021-07-31/Caminhao.java
@@ -0,0 +1,22 @@
+public class Caminhao implements Veiculo,Motor{
+
+    @Override
+    public String getNome() {
+        return null;
+    }
+
+    @Override
+    public int getId() {
+        return 0;
+    }
+
+    @Override
+    public String getModelo() {
+        return null;
+    }
+
+    @Override
+    public int getFabricante() {
+        return 0;
+    }
+}
---
+++ aula/2021-07-31/Onibus.java
@@ -0,0 +1,22 @@
+public class Onibus implements Veiculo,Motor{
+
+    @Override
+    public String getNome() {
+        return null;
+    }
+
+    @Override
+    public int getId() {
+        return 0;
+    }
+
+    @Override
+    public String getModelo() {
+        return null;
+    }
+
+    @Override
+    public int getFabricante() {
+        return 0;
+    }
+}
